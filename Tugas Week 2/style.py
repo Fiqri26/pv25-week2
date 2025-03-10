08f3ee3cb03c81cf034e6f7940f656d0eaa6625e
Fiqri26 <jordyardianto26@gmail.com>
2025-03-10 21:24:23 +0800

    tugas week dua

---
+++ Tugas Week 2/style.py
@@ -0,0 +1,36 @@
+STYLE_SHEET = """
+    QLabel {
+        font-weight: bold;
+    }
+    QGroupBox {
+        font-weight: bold;
+        border: 1px solid gray;
+        border-radius: 5px;
+        padding: 5px;
+        margin-top: 18px;
+    }
+    QGroupBox::title {
+        font-weight: bold;
+        subcontrol-origin: margin;
+        padding: 0px 5px;
+        background-color: #2b2b2b;
+        color: white;
+    }
+    QPushButton {
+        font-weight: bold;
+        background-color: #6495ED;
+        color: white;
+        border-radius: 5px;
+        padding: 5px;
+        font-size: 14px;
+    }
+    QPushButton:hover {
+        background-color: #ff6666;
+    }
+    QLineEdit, QComboBox {
+        font-weight: bold;
+        border: 1px solid gray;
+        border-radius: 3px;
+        padding: 3px;
+    }
+"""
